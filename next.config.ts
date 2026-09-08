4e6749ca3cc4e03df2711bc39bcb51b7562613c4
Zah-id <zahidpulo53@gmail.com>
2026-09-08 08:17:20 +0700

    chore: rollback next.config.ts for local dev

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/UI-UX',
-  assetPrefix: '/UI-UX/',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
